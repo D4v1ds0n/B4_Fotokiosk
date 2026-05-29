66133358c99f2e336a8f3dd36860610dff6db2d6
erikvdaalen <90504704+erikvdaalen@users.noreply.github.com>
2026-05-29 13:52:56 +0200

    Foto's kunnen nu aan de cart worden toegevoegd

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 let photos = [];
+if (localStorage.getItem("cart")) {
+    cart = JSON.parse(localStorage.getItem("cart"));
+}
+else {
+    cart = [];
+    localStorage.setItem("ycart", JSON.stringify(cart));
+}
 
-document.getElementById("confirm-buy").addEventListener("click", function() {
-    document.getElementById("confirm-buy-overlay").style.display = "flex";
-});
+selectedPhoto = null;
+
 document.getElementById("close-confirm-buy-overlay").addEventListener("click", function() {
     document.getElementById("confirm-buy-overlay").style.display = "none";
 });
@@ -43,6 +49,13 @@ function loadPhotos() {
         photoInfo.textContent = info ? `#${info.id} - ${info.time}` : src;
         photoCard.appendChild(img);
         photoCard.appendChild(photoInfo);
+        photoCard.addEventListener("click", function() {
+            selectedPhoto = img.src;
+            document.getElementById("confirm-buy-overlay").style.display = "flex";
+            document.getElementById("confirm-buy-photo").src = img.src;
+            document.getElementById("confirm-buy-photo").alt = img.alt;
+            document.getElementById("confirm-buy-photo-info").textContent = photoInfo.textContent;
+        });
     });
 };
 
@@ -84,3 +97,30 @@ function parseFileName(fileName) {
     id: Number(id)
   };
 }
+
+document.getElementById("add-to-cart").addEventListener("click", function() {
+    if (selectedPhoto) {
+        const selectedSize = document.getElementById("photo-size").value;
+        cart.push({ photo: selectedPhoto, size: selectedSize });
+        localStorage.setItem("cart", JSON.stringify(cart));
+        alert("Foto toegevoegd aan winkelwagentje");
+        document.getElementById("confirm-buy-overlay").style.display = "none";
+    }
+});
+
+document.getElementById("photo-size").addEventListener("change", function() {
+    const selectedSize = this.value;
+    let price = 0;
+    switch (selectedSize) {
+        case "small":
+            price = 3.00;
+            break;
+        case "medium":
+            price = 5.00;
+            break;
+        case "large":
+            price = 7.00;
+            break;
+    }
+    document.getElementById("price").textContent = `€ ${price.toFixed(2)}`;
+});
